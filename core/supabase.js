5ae307d83c577fdcfde75c32eded9806553cba8e
aluriz <97389551+aluriz@users.noreply.github.com>
2026-06-26 19:11:15 +0200

    Update supabase.js

--- core/supabase.js
+++ core/supabase.js
@@ -2,7 +2,6 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 const SUPABASE_URL = "https://oicztqxxgtnotlmglexp.supabase.co";
 
-// Tu publishable key (anon/public key moderna)
-const SUPABASE_ANON_KEY = "sb_publishable_snae7lZbK79TP7OyluC7gg_rg28FPLP";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9pY3p0cXh4Z3Rub3RsbWdsZXhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0ODIxMzUsImV4cCI6MjA5ODA1ODEzNX0.GipSAoYdmwqRD8w14fmXlhJsxS7AZhqRNwi4HVhDuyw";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
